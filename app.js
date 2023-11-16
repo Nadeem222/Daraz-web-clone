6727443b8dfeb34464b26894fea696d88fc8f11f
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2023-11-16 15:09:53 +0500

    app complete

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function showSlides() {
     }
 
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    setTimeout(showSlides, 5000); 
 }
 
 function plusSlides(n) {
@@ -23,3 +23,28 @@ function plusSlides(n) {
 }
 
 document.addEventListener("DOMContentLoaded", showSlides);
+
+
+
+var saleEndTime = new Date('November 17, 2023 12:00:00 GMT+0000');
+
+function updateTimer(){
+    var currenttimer = new Date ();
+
+    var differnceTimer = saleEndTime - currenttimer;
+
+    if(differnceTimer > 0 ){
+        var hours = Math.floor(differnceTimer / (1000 * 60 * 60));
+
+        var minutes = Math.floor((differnceTimer % (1000* 60 * 60  )) / (1000 * 60));
+        var second = Math.floor((differnceTimer % (1000 * 60 )) / 1000);
+
+        document.getElementById("hours").innerHTML =  hours
+        document.getElementById("minutes").innerHTML =  minutes
+        document.getElementById("second").innerHTML =  second
+    }
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
